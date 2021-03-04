74820b70d04c203a5365e6a7d8edf4b3483ae93c
Slavacss <tihon4326@mail.ru>
2021-03-04 13:38:41 +0400

    1

--- R440O/TestModule/TestResultForm.Designer.cs
+++ R440O/TestModule/TestResultForm.Designer.cs
@@ -80,7 +80,7 @@
             // 
             this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(148, 146);
+            this.button1.Location = new System.Drawing.Point(148, 131);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(106, 41);
             this.button1.TabIndex = 4;
